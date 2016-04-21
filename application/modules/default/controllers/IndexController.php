30e94953d0d68aece9fa59d779c6e79d6aa5a496
SierraChest <SierraChest@users.noreply.github.com>
2016-04-21 19:35:19 +0200

    Added files via upload

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -5,11 +5,50 @@ class IndexController extends Sierra_Controller_Action
 
     public function indexAction()
     {
-
     }
 
+    public function sierraMagazinesAction()
+    {
+    }
+    
+    public function catalogsAction()
+    {
+    }
+    
+    public function buyersGuidesAction()
+    {
+    }
+    
+    public function tsnTimesAction()
+    {
+    }
+    
+    public function gameSearchAction()
+    {
+    }
+    
+    public function demosAction()
+    {
+    }
+    
+    public function trailersAction()
+    {
+    }
+    
+    public function booksAction()
+    {
+    }
+    
+    public function merchandiseAction()
+    {
+    }
+    
     public function aboutUsAction()
     {
-
     }
+    
+    public function linksAction()
+    {
+    }
+
 }
